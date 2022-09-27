239f9f3f196d7833c698297da39d464185b5a6ab
Loic Allemand <loic.allemand@etu.univ-grenoble-alpes.fr>
2022-09-27 14:10:01 +0200

    player class

--- Data/Player.java
+++ Data/Player.java
@@ -1,5 +1,67 @@
 package Data;
 
+import java.util.ArrayList;
+
 public class Player {
 
+    private String name;
+    private ArrayList<Town> townList = new ArrayList<Town>();
+
+    private int silver;
+    private int gold;
+
+    public Player(String name){
+        this.name = name;
+        townList.add(new Town(this, name));
+        this.gold = 0;
+        this.silver = 0;
+    }
+
+    public Player(String name, int silver, int gold){
+        this.name = name;
+        townList.add(new Town(this, name));
+        this.gold = gold;
+        this.silver = silver;
+    }
+
+    /**
+     * Player name management
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Player town management
+     */
+    public ArrayList<Town> getTownList() {
+        return townList;
+    }
+
+    public void addTown(Town town) {
+        this.townList.add(town);
+    }
+
+    /**
+     * Player money management
+     */
+    public int getGold() {
+        return gold;
+    }
+
+    public int getSilver() {
+        return silver;
+    }
+
+    public void addGold(int gold) {
+        this.gold =+ gold;
+    }
+
+    public void addSilver(int silver) {
+        this.silver =+ silver;
+    }
 }
