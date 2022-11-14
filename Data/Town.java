3b9b200d50c1f98a187ce4fbd0bb7022d3f36d48
Loic Allemand <loic.allemand@etu.univ-grenoble-alpes.fr>
2022-11-14 13:11:04 +0100

    ressources conversion

--- Data/Town.java
+++ Data/Town.java
@@ -49,12 +49,67 @@ public class Town {
     /**
      *  Town ressources management
      */
+
+    public HashMap<Ressources, Integer> getRessources() {
+        return ressources;
+    }
+
+    public int getRessources(Ressources res) {
+        return getRessources().get(res);
+    }
+    
     public void addRessources(Ressources res, int nbRessources) {
         this.ressources.put(res, this.ressources.get(res)+nbRessources);
     }
 
     public void removeRessources(Ressources res, int nbRessources) {
         this.ressources.put(res, this.ressources.get(res)-nbRessources);
+    }
+
+    public int getMaxConvertToAdvancedRessources(AdvancedRessources advRes){
+        switch (advRes) {
+            case AD1:
+                return (int) Math.floor( Math.min(getRessources(Ressources.M1)/50,getRessources(Ressources.M4)/50) );
+            case AD2:
+                return (int) Math.floor( Math.min(getRessources(Ressources.M2)/50,getRessources(Ressources.M5)/50) );
+            case AD3:
+                return (int) Math.floor( Math.min(getRessources(Ressources.M3)/50,getRessources(Ressources.M6)/50) );
+        
+            default:
+                System.out.println("Error - This type doesn't exist");
+                return -1;
+        }
+    }
+
+    public void convertToAdvancedRessources(AdvancedRessources advRes, int nbRessources){
+        switch (advRes) {
+            case AD1:
+                if (nbRessources < getMaxConvertToAdvancedRessources(advRes)) {
+                    removeRessources(Ressources.M1, nbRessources*50);
+                    removeRessources(Ressources.M4, nbRessources*50);
+                    break;
+                }
+
+            case AD2:
+                if (nbRessources < getMaxConvertToAdvancedRessources(advRes)) {
+                    removeRessources(Ressources.M2, nbRessources*50);
+                    removeRessources(Ressources.M5, nbRessources*50);
+                    break;
+                }
+
+            case AD3:
+                if (nbRessources < getMaxConvertToAdvancedRessources(advRes)) {
+                    removeRessources(Ressources.M3, nbRessources*50);
+                    removeRessources(Ressources.M6, nbRessources*50);
+                    break;
+                }
+                System.out.println("Error - The town don't have the ressources needed");
+                break;
+
+            default:
+                System.out.println("Error - This type of ressources doesn't exist");
+                break;
+        }
     }
 
     /**
